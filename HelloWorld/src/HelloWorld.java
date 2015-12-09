87949429a01320c709b18774b4b599adb6098303
Sumit Dige <Sumit Dige>
2015-12-09 12:51:02 +0530

    Initial upload...

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is first version of Hello World...");
+
+	}
+
+}
