1c2240c87c2a12404cb4f66c8faef029e887b67e
Sumit Dige <Sumit Dige>
2015-12-09 12:57:07 +0530

    user accepted input by $um!T

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,13 +1,22 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class HelloWorld {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		String name;
 		
-		System.out.println("This is first version of Hello World...");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter your name");
+		name = br.readLine();
+		
+		System.out.println("Hello "+name);
 
 	}
 
